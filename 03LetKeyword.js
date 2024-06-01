45084af4c14497ecf229ea6b4e7a116819cfbe6d
HamzaAliUddin <hamzaalikhan20611@gmail.com>
2024-06-01 15:21:32 -0700

    let keyword varaible

---
+++ 03LetKeyword.js
@@ -0,0 +1,12 @@
+// let keyword
+// declare varaible with let
+
+// use let keyword mostly 
+// let varaible value we change it but we can not declear let with again same varaible name 
+
+let first_Name= "Hamza" ;
+console.log(first_Name);
+    // let first_Name ="ali"; //(invalid)
+first_Name ="ali"; //(valid)
+console.log(first_Name)
+
