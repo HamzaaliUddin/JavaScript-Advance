053db48e2af3f632bb0b126d1387e6321a650beb
HamzaAliUddin <hamzaalikhan20611@gmail.com>
2024-06-01 15:33:48 -0700

    indexing and length

---
+++ 05StringLengthIndexing.js
@@ -0,0 +1,14 @@
+// String Index
+// it always start from  0 index and so on
+
+let firstName = 'Hamza';
+let stringIndex = firstName[2];
+console.log(stringIndex)
+
+// String length
+// it always start from  1 index and so on and last index -1
+
+let lastName = "Ali";
+let lengthIndex = lastName.length;
+let lengthIndexEndPoint = lastName[lastName.length - 1]; //to get last index
+console.log(lengthIndexEndPoint);
