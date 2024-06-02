2cbcc95f508a48c49af1cfc7a45e4070517fd409
HamzaAliUddin <hamzaalikhan20611@gmail.com>
2024-06-02 11:15:30 -0700

    Reference Data Type Array

---
+++ 22Array.js
@@ -0,0 +1,10 @@
+//  Reference Data Type is object  
+// Array  
+
+let fruits =["apple","banana","grapes","orange"];
+console.log(fruits);
+fruits[2]= "Mango";
+console.log(fruits)
+console.log(typeof(fruits))
+// to know it is array or not 
+console.log(Array.isArray(fruits)) 
