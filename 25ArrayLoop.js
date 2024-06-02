73c6ab62feace9f70b8f380fba82cfabec76614a
HamzaAliUddin <hamzaalikhan20611@gmail.com>
2024-06-02 11:51:48 -0700

    array with for loop

---
+++ 25ArrayLoop.js
@@ -0,0 +1,12 @@
+
+// Array with For loop;
+
+
+let item1 = ["item1","item2","item3"];
+let item2 = ["item4","item5","item6"];
+let item = [...item1,...item2];
+
+
+for(i = 0; i<= item.length - 1; i++){
+    console.log(item[i]);
+}
