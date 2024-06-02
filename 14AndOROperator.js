7dc5fa63991c4d7cfe80ff2d961ec9b846bab672
HamzaAliUddin <hamzaalikhan20611@gmail.com>
2024-06-02 00:38:08 -0700

    && and || operator

---
+++ 14AndOROperator.js
@@ -0,0 +1,19 @@
+//  and or  operator
+
+
+let firstName = "Hamza";
+let age =10;
+
+if(firstName[2] === "m" && firstName === firstName.toString()){
+    console.log(firstName);
+}else{
+    console.log("wrong")
+}
+
+//  || OR Operator
+// if one of each condition wil true so it will true
+if(age>=18 || firstName === firstName.toString()){
+    console.log(firstName);
+}else{
+    console.log("wrong")
+}
