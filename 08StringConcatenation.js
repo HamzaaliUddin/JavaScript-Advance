66f7573f2d5356b7598d152a5f2b8d90ebac39d9
HamzaAliUddin <hamzaalikhan20611@gmail.com>
2024-06-01 17:22:05 -0700

    String Concatenation

---
+++ 08StringConcatenation.js
@@ -0,0 +1,11 @@
+// String concatenation
+
+
+
+let first = "10";
+let last = "50";
+
+let fullname = first + last
+let addNumber = +first + +last //
+console.log(fullname,addNumber);
+console.log(typeof(fullname),typeof(addNumber));
