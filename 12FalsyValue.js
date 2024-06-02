a1bab0e86c218ef8387c4d54349890516ae77ac8
HamzaAliUddin <hamzaalikhan20611@gmail.com>
2024-06-02 00:16:54 -0700

    truthy value and falsy value

---
+++ 12FalsyValue.js
@@ -0,0 +1,25 @@
+// falsy value
+
+// ""
+// undefind
+// null
+// false
+// 0
+
+// truthy value 
+
+// "abc"
+// 1, -1 
+
+let falseValue =""; // 0 false, null , undefind
+if(falseValue){
+    console.log("truth value");
+}else{
+    console.log('falsy value')
+}
+let trueValue =-1;
+if(trueValue){
+    console.log("truth value");
+}else{
+    console.log('falsy value')
+}
