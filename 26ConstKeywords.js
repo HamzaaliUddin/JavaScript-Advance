3ceb0d7adcde73f57aa66fdca1123117d6ae21df
HamzaAliUddin <hamzaalikhan20611@gmail.com>
2024-06-02 11:58:32 -0700

    const keyword with array

---
+++ 26ConstKeywords.js
@@ -0,0 +1,12 @@
+
+// use const for creating array
+// is better use const with reference Data Type
+
+const fruits = ["apple","Banana","mango"];
+// fruits=["apple","Banana","mango",'watermelon'] // (invalid) with const  so don't use it with const
+fruits.push("orange");
+console.log(fruits)
+
+let fruitItems = ["apple","Banana","mango"];
+fruitItems=["apple","Banana","mango",'watermelon']
+console.log(fruitItems)
