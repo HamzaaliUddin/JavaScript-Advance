51efbb8ab131cb0dc4288fdcfa9a40c9af55ded1
HamzaAliUddin <hamzaalikhan20611@gmail.com>
2024-06-02 10:21:57 -0700

    WhileLoop Example

--- 18WhileLoop.js
+++ 18WhileLoop.js
@@ -1,11 +1,31 @@
 // While Loop
 
 
-let WhileLoop = 0;
+// let WhileLoop = 0;
 
-let user_num = +prompt("How Many Times?")
+    let user_num = +prompt("How Many Times?")
 
-while (WhileLoop < user_num) {
-    WhileLoop++;
-    console.log(WhileLoop,"I lOVE you");
-}
+    while (WhileLoop < user_num) {
+        WhileLoop++;
+        console.log(WhileLoop,"I lOVE you");
+    }
+
+// Example 
+
+
+    let i = 0;
+    let total = 0;
+
+
+    while (i<= 10) {
+        total = total +  i;
+        i++;
+    }
+    console.log(total)
+
+// Or 
+
+    let num =10;
+
+    let naturalNumberOfSum = (num*(num+1))/2;
+    console.log(naturalNumberOfSum)
