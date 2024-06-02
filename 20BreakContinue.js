927eb0bf0bdf2c80382565b5368decc303278210
HamzaAliUddin <hamzaalikhan20611@gmail.com>
2024-06-02 10:58:50 -0700

    for loop break and continue

---
+++ 20BreakContinue.js
@@ -0,0 +1,23 @@
+
+
+// Break and Continue
+
+
+let num = 1;
+
+for(num; num<=10;num++){
+    if(num === 3){
+        break;
+    }
+    console.log(num);
+}
+
+console.log("   ")
+let i = 1;
+
+for(i; i<=10;i++){
+    if(i === 3){
+        continue;
+    }
+    console.log(i);
+}
