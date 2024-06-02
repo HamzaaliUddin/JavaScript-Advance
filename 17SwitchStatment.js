eacd6ed0a889a03c4b64b0435d2b21040297b0db
HamzaAliUddin <hamzaalikhan20611@gmail.com>
2024-06-02 10:03:52 -0700

    Switch Statment

---
+++ 17SwitchStatment.js
@@ -0,0 +1,30 @@
+// Switch Staatment 
+
+
+let day = 2;
+
+switch (day){
+    case 0:
+        console.log("day 1");
+        break;
+    case 1:
+        console.log("day 2");
+        break;
+    case 2:
+        console.log("day 3");
+        break;
+    case 3:
+        console.log("day 4");
+        break;
+    case 4:
+        console.log("day 5");
+        break;
+    case 5:
+        console.log("day 6");
+        break;
+    case 6:
+        console.log("day 7");
+        break;
+    default:
+        console.log("invalid day")
+}
