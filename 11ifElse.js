2b86da0c8f44bd9e0409ba7db6207daeee2c6208
HamzaAliUddin <hamzaalikhan20611@gmail.com>
2024-06-02 00:11:57 -0700

    if else statement

---
+++ 11ifElse.js
@@ -0,0 +1,18 @@
+// if else statment
+
+
+let age =18;
+
+if(age>=18){
+    console.log("you can play this game!!")
+}else{
+    console.log("you can not play this game!!")
+}
+
+let num = 4;
+
+if(num %2 === 0){
+    console.log("even");
+}else{
+    console.log("odd");
+}
