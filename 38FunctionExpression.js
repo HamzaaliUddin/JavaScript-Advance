a5eaa27edbc44d627154b184849f785d0d3e913f
HamzaAliUddin <hamzaalikhan20611@gmail.com>
2024-06-03 06:37:23 -0700

    function expression

---
+++ 38FunctionExpression.js
@@ -0,0 +1,17 @@
+// Function Expression
+// store in var let or const
+
+
+
+
+const happyBirthday = function wishHappyBirthday() {
+    console.log("happy birthday to you.....")
+}
+
+happyBirthday();
+happyBirthday();
+
+const isEven =function evenOdd(num){
+    return num % 2  === 0 ? 'Even':'odd';
+}
+console.log(isEven(2))
