c40ec28abc77bc166cc7b662128172b1e54a85b3
HamzaAliUddin <hamzaalikhan20611@gmail.com>
2024-06-03 14:24:30 -0700

    Param Destrucutrning

---
+++ 45ParamDestructurning.js
@@ -0,0 +1,20 @@
+// param Destructurning
+
+
+
+
+const obj ={
+    name :'Hamza',
+    age:21,
+}
+
+    // function objFunc(obj){
+    //     console.log(obj.name);
+    // }
+// Destructruning
+    function objFunc({name,age}){
+        console.log(name);
+        console.log(age)
+    }
+
+objFunc(obj);
