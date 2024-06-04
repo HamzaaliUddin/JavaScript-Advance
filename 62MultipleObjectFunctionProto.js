a6909822a0f6e933c488714908b15715e2adce25
HamzaAliUddin <hamzaalikhan20611@gmail.com>
2024-06-04 09:24:22 -0700

    Multiple Object using function and __proto__

---
+++ 62MultipleObjectFunctionProto.js
@@ -0,0 +1,31 @@
+// Mutliple Object using function
+
+
+const userMethod ={
+    about: function(){
+        console.log(`My name is ${this.firstName}`)
+        return `my Name is ${this.firstName}`
+    },
+    party: function(){
+        return 'Party Data has been extened'    
+    }
+}
+
+function createUser(firstName,lastName,age,address){
+    // __proto__
+    const user = Object.create(userMethod);
+    user.firstName = firstName,
+    user.lastName = lastName,
+    user.age = age,
+    user.address = address
+    return user
+}
+
+const user1 = createUser("hamza","ali",21,'Pakistan');
+const user2 = createUser("ali","abbas",34,'karachi');
+
+console.log(user1)
+console.log(user2)
+console.log(user2.party())
+// calling __proto__
+console.log(user2.__proto__)
