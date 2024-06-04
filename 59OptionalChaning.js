5e00b728864b28089bcfe8f13979f49d92369035
HamzaAliUddin <hamzaalikhan20611@gmail.com>
2024-06-04 06:22:17 -0700

    Optinal Chaning ?

---
+++ 59OptionalChaning.js
@@ -0,0 +1,9 @@
+// Optinal Chaning object
+// use ? is better than error it wil return undefined
+
+const user ={
+    firstName:"coder",
+    // address:{houseNumber:'123'},
+}
+
+console.log(user.address?.houseNumber);
