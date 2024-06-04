441d0b1a0d6e5af2deaaabf4866bbe926a24a847
HamzaAliUddin <hamzaalikhan20611@gmail.com>
2024-06-03 23:02:49 -0700

    splice method

---
+++ 54Splice.js
@@ -0,0 +1,8 @@
+// splide start,delete,insert
+
+
+const myArray = ['item1','item2','item3','item4'];
+
+const deleteItem = myArray.splice(1,2,{name:"Hamza"})
+console.log(`delected item ${deleteItem}`)
+console.log(myArray)
